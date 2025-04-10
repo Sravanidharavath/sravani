b7e7a0b5dcd4c7dc7a159ab3d85018e2c958c47d
Sravanidharavath <sravanidharavath332@gmail.com>
2025-04-10 13:57:21 +0500

    Update abc.java

--- abc.java
+++ abc.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class abc {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("welcome to all");
     }
 }
